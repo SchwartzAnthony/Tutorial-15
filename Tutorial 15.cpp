576a70f9d472792e7f7905a009c64c50ad159e05
SchwartzAJ <schwartzanthonyj@gmail.com>
2023-02-06 09:28:11 +0100

    Add project files.

---
+++ Tutorial 15.cpp
@@ -0,0 +1,28 @@
+// Tutorial 15.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+// Internal Debugger tutorial 
+
+#include <iostream>
+
+int readNumber(int x)
+{
+	std::cout << "Please enter a number: ";
+	std::cin >> x;
+	return x;
+}
+
+void writeAnswer(int x)
+{
+	std::cout << "The sum is: " << x << '\n';
+}
+
+int main()
+{
+	int x{};
+	x = readNumber(x) + readNumber(x);
+	writeAnswer(x);
+
+	return 0;
+}
